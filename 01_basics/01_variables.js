44d13be5f4ed968f846e681b8b2e4717d0b3173c
Inderjeet Bhagat <62701663+inderjeetbhagat@users.noreply.github.com>
2023-11-20 10:56:20 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 1234
+let accountEmail = "hitesh@google.com"
+var accountPassword = "1231"
+accountCity = "Kolkata"
+let accountState;
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+console.log(typeof(accountCity));
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello"); // using node.js not browser
+
+//code readability should be high
+let name = "Inder"
+let age = 23
+let isLoggedIn = false
+
+
+/*
+Primitive Data Types
+number => 2 to power 53
+bigint
+string => ""
+boolean => true/false
+null => standalone value // it is an object
+undefined => vlaue is not yet assigned // undefined type
+symbol => uniqueness
+
+Objects
+*/
+
