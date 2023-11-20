8a6a1a65aee73b3a5ca7dd2f17197657fb24b329
Inderjeet Bhagat <62701663+inderjeetbhagat@users.noreply.github.com>
2023-11-20 11:09:10 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,15 @@
+let score = 33
+
+console.log(typeof(score));
+
+
+let valueInNumber = Number(score);
+console.log(typeof(valueInNumber));
+
+/*
+"33" => 33
+"33abc" => NaN
+true => 1
+null => 0
+false => 0 
+*/
